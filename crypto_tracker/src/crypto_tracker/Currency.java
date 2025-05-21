1c33ac628fd62810a9fb9a132ce19c30413656fe
Sambor Smigiel <sambor.smigiel@sazusolution.com>
2025-05-21 14:31:28 +0200

    Getters, setters and constructor for Currency

--- crypto_tracker/src/crypto_tracker/Currency.java
+++ crypto_tracker/src/crypto_tracker/Currency.java
@@ -13,4 +13,69 @@ public class Currency {
 	private float retrievedUsdt;
 	private float profit;
 	
+	public Currency(String name, float averageBuyPrice, float averageSellPrice, float amountOwned, 
+					float investedUsdt, float retrievedUsdt, float profit) {
+		super();
+		this.name = name;
+		this.averageBuyPrice = averageBuyPrice;
+		this.averageSellPrice = averageSellPrice;
+		this.amountOwned = amountOwned;
+		this.investedUsdt = investedUsdt;
+		this.retrievedUsdt = retrievedUsdt;
+		this.profit = profit;
+	}
+
+	public List<DataEntry> getDataEntries() {
+		return dataEntries;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public float getAverageBuyPrice() {
+		return averageBuyPrice;
+	}
+	public void setAverageBuyPrice(float averageBuyPrice) {
+		this.averageBuyPrice = averageBuyPrice;
+	}
+	
+	public float getAverageSellPrice() {
+		return averageSellPrice;
+	}
+	public void setAverageSellPrice(float averageSellPrice) {
+		this.averageSellPrice = averageSellPrice;
+	}
+	
+	public float getAmountOwned() {
+		return amountOwned;
+	}
+	public void setAmountOwned(float amountOwned) {
+		this.amountOwned = amountOwned;
+	}
+	
+	public float getInvestedUsdt() {
+		return investedUsdt;
+	}
+	public void setInvestedUsdt(float investedUsdt) {
+		this.investedUsdt = investedUsdt;
+	}
+	
+	public float getRetrievedUsdt() {
+		return retrievedUsdt;
+	}
+	public void setRetrievedUsdt(float retrievedUsdt) {
+		this.retrievedUsdt = retrievedUsdt;
+	}
+	
+	public float getProfit() {
+		return profit;
+	}
+	public void setProfit(float profit) {
+		this.profit = profit;
+	}
+	
 }
