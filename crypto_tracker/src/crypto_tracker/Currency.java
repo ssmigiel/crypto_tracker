2471437a295c174cd8ae1b1adabe981fe0b1192e
Sambor Smigiel <sambor.smigiel@sazusolution.com>
2025-05-21 14:21:07 +0200

    Currency class fields

--- crypto_tracker/src/crypto_tracker/Currency.java
+++ crypto_tracker/src/crypto_tracker/Currency.java
@@ -4,4 +4,13 @@ import java.util.List;
 
 public class Currency {
 	private List<DataEntry> dataEntries;
+	
+	private String name;
+	private float averageBuyPrice;
+	private float averageSellPrice;
+	private float amountOwned;
+	private float investedUsdt;
+	private float retrievedUsdt;
+	private float profit;
+	
 }
